a1a85cd8ab430e201f328fd4aa221c8ca8c903ca
Ruth Acosta <ruthacosta100@gmail.com>
2016-01-25 12:36:46 -0500

    fixed @@ again

--- server.rb
+++ server.rb
@@ -21,7 +21,7 @@ class Server < Sinatra::Base
 
       def current_user
         if session["user_id"]
-          @users ||= @@db.exec_params(<<-SQL, [session["user_id"]]).first
+          @users ||= db.exec_params(<<-SQL, [session["user_id"]]).first
             SELECT * FROM users WHERE id = $1
           SQL
         else
@@ -43,7 +43,7 @@ class Server < Sinatra::Base
 
       post "/signup" do
   	    encrypted_password = BCrypt::Password.create(params[:user_password])
-        users = @@db.exec_params(<<-SQL, [params[:user_name], params[:user_email],encrypted_password]) 
+        users = db.exec_params(<<-SQL, [params[:user_name], params[:user_email],encrypted_password]) 
         INSERT INTO users (user_name, user_email, user_password_digest) VALUES ($1, $2, $3) RETURNING id;
       SQL
 
@@ -58,7 +58,7 @@ class Server < Sinatra::Base
 	
 
       post "/loggedin" do
-        @user = @@db.exec_params("SELECT * FROM users WHERE user_name = $1", [params[:user_name]]).first
+        @user = db.exec_params("SELECT * FROM users WHERE user_name = $1", [params[:user_name]]).first
         if @user
           if BCrypt::Password.new(@user["user_password_digest"]) == params[:user_password]	
             session["user_id"] = @user["id"]
@@ -76,8 +76,8 @@ class Server < Sinatra::Base
 
       get "/topics" do 
         if session["user_id"]
-      	   @@db = PG.connect(dbname: "forum_project")
-      	   @topics = @@db.exec("SELECT topic FROM topics").to_a
+      	   db = PG.connect(dbname: "forum_project")
+      	   @topics = db.exec("SELECT topic FROM topics").to_a
   	  	  erb :topics
         else
           "not logged in"
@@ -88,8 +88,8 @@ class Server < Sinatra::Base
 	   post "/topics" do
         user_id = session['user_id'].to_i
         topic = params['topic']
-        @@db = PG.connect(dbname: "forum_project")
-        new_topic = @@db.exec_params("INSERT INTO topics (user_id, topic) 
+        db = PG.connect(dbname: "forum_project")
+        new_topic = db.exec_params("INSERT INTO topics (user_id, topic) 
                                     VALUES ($1, $2) RETURNING id",[user_id,topic])   
         topic_submitted = true
 	  	
@@ -97,7 +97,7 @@ class Server < Sinatra::Base
 	   end
 
      get "/topics/:id" do 
-        @posts = @@db.exec_params("SELECT posts.*, users.user_name FROM posts 
+        @posts = db.exec_params("SELECT posts.*, users.user_name FROM posts 
                                  LEFT JOIN users ON posts.user_id = users.id 
                                  WHERE topic_id = $1",[params[:id]])
         erb :posts
@@ -107,8 +107,8 @@ class Server < Sinatra::Base
       user_id = session['user_id'].to_i
       topic_id = params['topic_id']
       content = params['content']
-      @@db = PG.connect(dbname: "forum_project")
-      new_post = @@db.exec_params("INSERT INTO posts (user_id, topic_id, content) 
+      db = PG.connect(dbname: "forum_project")
+      new_post = db.exec_params("INSERT INTO posts (user_id, topic_id, content) 
                         VALUES ($1, $2, $3) RETURNING id",[user_id, topic_id, content])
       
       # content_submitted = true
@@ -123,7 +123,7 @@ class Server < Sinatra::Base
      # end
      
      get "/comments" do
-        @comments = @@db.exec_params("SELECT comments.*, users.user_name FROM comments 
+        @comments = db.exec_params("SELECT comments.*, users.user_name FROM comments 
                                   LEFT JOIN users ON comments.user_id = users.id
                                   WHERE post_id = $1",[params[:id]])
         erb :comments
@@ -136,8 +136,8 @@ class Server < Sinatra::Base
         comment = params['comment']
         count = params['count']    
       
-        @@db = PG.connect(dbname: "forum_project")
-        new_content = @@db.exec_params("INSERT INTO comments (user_id, topic_id, post_id, comment, count) 
+        db = PG.connect(dbname: "forum_project")
+        new_content = db.exec_params("INSERT INTO comments (user_id, topic_id, post_id, comment, count) 
                                     VALUES ($1, $2, $3, $4, $5) RETURNING id",[user_id, topic_id, post_id, comment, count])   
         content_submitted = true
       
